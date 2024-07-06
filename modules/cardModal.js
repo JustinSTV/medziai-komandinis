1addc9eeb1e4aa3a9ca590f2057433bbd52ab1f0
Justin S <steponaviciusjustas@gmail.com>
2024-07-06 12:38:07 +0300

    Modalas veikia ir atsidaro

--- modules/cardModal.js
+++ modules/cardModal.js
@@ -21,10 +21,28 @@ export default class CardModal {
       const infoField = document.createElement('div')
       infoField.classList.add('infoField')
 
-      const treeName = document.createElement('h1');
+      const treeName = document.createElement('h2');
       treeName.textContent = this.props.name;
+
+      const treeSpecies = document.createElement('p');
+      treeSpecies.textContent = `Rušis: ${this.props.species}`
+
+      const treeAge = document.createElement('p');
+      treeAge.textContent = `Amžius: ${this.props.age_years}`
+
+      const treeHeight = document.createElement('p');
+      treeHeight.textContent = `Aukštis: ${this.props.height_meters} m.`
+
+      const treeDiameter = document.createElement('p');
+      treeDiameter.textContent = `Skersmuo: ${this.props.diameter_cm} cm`
+
+      const treeSpread = document.createElement('p');
+      treeSpread.textContent = `Medžių paplitimas: ${this.props.spread}`
+
+
       const treeDescription = document.createElement('p');
       treeDescription.textContent = this.props.features;
+
 
       const closeButton = document.createElement('button');
       closeButton.textContent = 'Close';
@@ -33,7 +51,7 @@ export default class CardModal {
          cardModal.close();
       });
 
-      infoField.append(treeName, treeDescription)
+      infoField.append(treeName, treeDescription, treeSpecies, treeAge, treeHeight, treeDiameter, treeSpread)
 
       mainModalDiv.append(treeImage, infoField)
       cardModal.append(mainModalDiv, closeButton);
