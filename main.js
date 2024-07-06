f10197f9509613e054fb3f09e63fe919fbdcd34b
Justin S <steponaviciusjustas@gmail.com>
2024-07-06 13:03:48 +0300

    Code clean up

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
 import CardModal from "./modules/CardModal.js";
 
-//?? aprašytas modalas
-// fetch("trees.json")
-//    .then((res) => res.json())
-//    .then((data) => {
-//       const mainSec = document.querySelector("#mainSec");
-
-//       data.forEach((element) => {
-//          console.log(element);
-
-//          const oneCard = document.createElement("div");
-//          oneCard.classList.add("card");
-
-//          const cardName = document.createElement("h1");
-//          cardName.textContent = element.name;
-
-//          const cardModalBtn = document.createElement("button");
-//          cardModalBtn.textContent = "More Info";
-
-//          cardModalBtn.addEventListener("click", () => {
-//             const modal = new CardModal(element);
-//             document.body.append(modal);
-//             modal.showModal();
-//          });
-
-//          oneCard.append(cardName, cardModalBtn);
-//          mainSec.append(oneCard);
-//       });
-//    });
 
 // garso grojimas
 
